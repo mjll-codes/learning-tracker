da0246da46c6cc6eb729b58d9358ea52f4a5815b
Madison Liddell <77262760+mjll-codes@users.noreply.github.com>
2021-10-02 19:04:30 +1000

    Update Profile-Lookup.js

--- learning-outcomes/Profile-Lookup.js
+++ learning-outcomes/Profile-Lookup.js
@@ -1,3 +1,42 @@
+//A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+//The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+//If both are true, then return the "value" of that property.
+
+//If name does not correspond to any contacts then return the string No such contact.
+
+//If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
 function lookUpProfile(name, prop) {
   // Only change code below this line
 
