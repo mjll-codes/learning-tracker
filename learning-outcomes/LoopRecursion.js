d1325806de9877e6e9e8e877643d2735d9c08a29
Madison Liddell <77262760+mjll-codes@users.noreply.github.com>
2021-10-02 23:10:46 +1000

    Update LoopRecursion.js

--- learning-outcomes/LoopRecursion.js
+++ learning-outcomes/LoopRecursion.js
@@ -16,3 +16,15 @@ if (n <= 0) {
   return 0;
 } else {
   return sum(arr, n - 1) + arr[n - 1];
+
+// Explanation (using the above):
+  
+console.log(sum([2, 4, 6, 8, 10], ))
+
+      return sum([2, 4, 6, 8, 10], 3 - 1) + arr[3 - 1];
+        return sum ([2, 4, 6, 8, 10], 2)  + 6;
+          return sum ([2, 4, 6, 8, 10], 2 - 1) + 6;
+          return sum ([2, 4, 6, 8, 10], 1) + 4 (+ 6 );
+            return sum ([ 2, 4, 6, 8, 10], 1 - 1) + 2 (+ 4 + 6)
+              return sum ([2, 4, 6, 8, 10], 0) (+2 +4 +6)
+                return 2 + 4 + 6 = 12
